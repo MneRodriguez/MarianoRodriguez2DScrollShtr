e3c2318e2673988c1971ef326a2f77d914574c5f
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-05-09 16:31:02 -0300

    Initial commit

---
+++ Assets/Scripts/ScrollDeUV.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollDeUV : MonoBehaviour
+{
+    public Vector2 speed;
+
+    private void LateUpdate()
+    {
+        GetComponent<Renderer>().material.mainTextureOffset = speed * Time.time;
+    }
+}
