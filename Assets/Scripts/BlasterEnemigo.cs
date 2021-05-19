d0a221e1277456264105dc7d6f47e45a0c10257f
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-05-19 17:49:42 -0300

    Disparo enemigo ahora se mueve (faltan colisiones+triggers)

--- Assets/Scripts/BlasterEnemigo.cs
+++ Assets/Scripts/BlasterEnemigo.cs
@@ -13,11 +13,13 @@ public class BlasterEnemigo : MonoBehaviour
     public float velDisparo = 15f;
     public Rigidbody2D rbody;
     
+    
     void Start()
     {
-        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
-        rBody.velocity = -transform.right * velDisparo; // EL "transform.right" LLEVA SIMBOLO NEGATIVO PARA QUE VAYA A LA IZQ, PORQUE NO HAY "transform.left"
-
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>(); // TOMA LA NAVE
+                
+        rBody.velocity = -transform.right * velDisparo; // EL "transform.right" LLEVA SIMBOLO NEGATIVO PARA QUE LA NAVE ENEMIGA VAYA A LA IZQ, PORQUE NO HAY "transform.left"
+        disparoEnem.transform.right = -transform.right * (velDisparo * 2); // CON ESTO INTENTABA QUE EL TIRO ENEMIGO VAYA MÁS RÁPIDO QUE LO QUE VIAJA LA NAVE ENEMIGA
         InvokeRepeating("DisparoEnemigo", retrasoDisparo, cadenciaDisparo /2); // DIVIDO POR 2 PARA QUE DISPAREN CON MAYOR FRECUENCIA
                 
     }
--- Assets/Scripts/ManejoDeColisiones.cs
+++ Assets/Scripts/ManejoDeColisiones.cs
@@ -17,11 +17,11 @@ public class ManejoDeColisiones : MonoBehaviour
         GameObject gameControllerObject = GameObject.FindWithTag("GameController");
         gameController = gameObject.GetComponent<GameController>();
                  
-        tomarJugador = gameObject.GetComponent<GameObject>(); // NO SE SI ERA "gameObject" CON MINUSCULA O CON MAYUSCULA PARA QUE TOME EL OBJ CORRESP
-        tomarDisparoJgdr = gameObject.GetComponent<GameObject>();
+        tomarJugador = gameObject.GetComponent<GameObject>(); // TOMA SPRITE NAVE JUGADOR
+        tomarDisparoJgdr = gameObject.GetComponent<GameObject>(); // TOMA SPRITE TIRO DEL JUGADOR
 
-        tomarEnemigo = gameObject.GetComponent<GameObject>();
-        tomarDisparoEnem = gameObject.GetComponent<GameObject>();
+        tomarEnemigo = gameObject.GetComponent<GameObject>(); // TOMA SPRITE NAVE ENEM
+        tomarDisparoEnem = gameObject.GetComponent<GameObject>(); // TOMA SPRITE TIRO ENEM
     }
 
     
@@ -34,13 +34,13 @@ public class ManejoDeColisiones : MonoBehaviour
     {
         if (tomarJugador.CompareTag("TiroEnem")) // SI AL PLAYER LO ALCANZA UN DISPARO ENEMIGO
         {
-            Destroy(other.gameObject);
+            Destroy(tomarJugador.gameObject);
             gameController.GameOver();
         }
 
         if (tomarEnemigo.CompareTag("TiroJgdr")) // SI LOS ENEMIGOS TOCAN AL DISPARO DEL PLAYER
         {
-            Destroy(other.gameObject);
+            Destroy(tomarEnemigo.gameObject);
             gameController.SumarScore(ValorScore);
         }
         
